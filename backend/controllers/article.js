b710a7807eecb1b063248ddd05e8ba447930e646
David Eduardo Gualteros <degualteros@poligran.edu.co>
2019-11-11 10:28:31 -0500

    Metodos get update y delete de articles

--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -23,58 +23,195 @@ var controller = {
 
   save: (req, res) => {
     
-    //recoger parametros por POST
+    //Recoger parametros por post
     var params = req.body;
     console.log(params);
-        
-    
-    //validar datos (validator)
-
-    try {
+    // Validar datos con validator
+    try{
       var validate_title = !validator.isEmpty(params.title);
       var validate_content = !validator.isEmpty(params.content);
 
-    } catch (err) {
+    }catch(err){
       return res.status(200).send({
-        error: "Datos invalidos o incompletos"
+        status: "error",
+        message: "Datos errados"
       });
     }
 
     if (validate_title && validate_content) {
-      //crear objeto a guardar
-      var article = new Article();
-      //asignar valores 
-      article.title = params.title;
-      article.content = params.content;
-      article.image = null;
+       console.log("Validacion correcta")
+    //Crear objeto a Guardar
+    var article =  new Article();
 
-      //guardar articulo
-      article.save((err, articleStored) => {
-        if (err || !articleStored) {
-          return res.status(404).send({
-            status: "error",
-            message: "Articulo no guardado !!"
-          });
-        }
+    //Asignar valores
+    article.title = params.title;
+    article.content = params.content;
+    article.image = null;
 
-        
-
-      });
-
-      //devolver respuesta
+    //Guardar el articulo
+    article.save((err, articleStored) =>{
+      if(err || !articleStored){
+        return res.status(404).send({
+          status: "error",
+          message: "El articulo no se ha Guardado!!!"
+        });
+      }
+    //Devolver respuesta
       return res.status(200).send({
         status: "sucess",
-        article
-
-      });
-
-    } else {
+        article: articleStored
+      }); 
+    });
+    }else{
       return res.status(200).send({
-        message: "los datos no son validos !!!"
+        message: "Los datos no son validos!!!"
+      });
+    }
+  },
+
+  getArticles: (req, res) => {
+    //last
+    var query = Article.find({});
+
+    var last = req.params.last;
+    console.log(last);
+
+    if(last || last != undefined){
+      query.limit(5);
+    }
+    
+    //Find 
+    query.sort("-_id").exec((err, articles) => {
+      if(err){
+        return res.status(500).send({
+          status: "error",
+          message: "Error al devolver los articulos"
+        });
+      }
+
+      if(!articles){
+        return res.status(200).send({
+          status: "error",
+          message: "No hay articulos a mostrar !!!"
+        });
+      }
+
+      return res.status(200).send({
+        status: "success",
+        articles
+      });
+    })
+
+  },
+
+  getArticle: (req, res) =>{
+
+    //regoger el id de la url
+    var articleId =  req.params.id;
+    //Comprobar que existe
+    if(!articleId || articleId == null){
+      return res.status(200).send({
+        status: "error",
+        message: "No existe el articulo"
       });
     }
 
-  }
+    //Buscar el articulo
+    Article.findById(articleId, (err, article) =>{
+
+      if (err || !article) {
+        return res.status(200).send({
+          status: "error",
+          message: "No existe el articulo"
+        });
+      }
+
+    //Devolverlo en JSON
+      return res.status(200).send({
+        status: "success",
+        article
+      });
+
+    });
+  },
+
+  update: (req, res) => {
+    //Recoger id de articulo por la URL
+    var articleId =  req.params.id;
+    //Recoger datos que llegan por put
+    var params =  req.body;
+
+    //Validar datos
+    try{
+      var validate_title = !validator.isEmpty(params.title);
+      var validate_content = !validator.isEmpty(params.content);
+    }catch(err){
+      return res.status(200).send({
+        status: "error",
+        message: "Faltan datos por enviar !!!"
+      });
+    }
+
+    if(validate_title && validate_content){
+      //Find and update
+      Article.findOneAndUpdate({_id: articleId}, params, {new: true}, (err, articleUpdated) =>{ 
+        if(err){
+          return res.status(500).send({
+            status: "error",
+            message: "Error al actualizar"
+          });
+        }
+
+        if(!articleUpdated){
+          return res.status(404).send({
+            status: "error",
+            message: "No existe el articulo !!!"
+          });
+        }
+
+        return res.status(500).send({
+          status: "sucess",
+          article: articleUpdated
+        });
+
+      });
+    }else{
+      //Devolver respuesta
+      return res.status(200).send({
+        status: "error",
+        message: "La validacion no es correcta"
+      });
+    }
+  },
+
+  delete: (req, res) => {
+    //Recoger el ID de la URL
+    var articleId = req.params.id;
+
+    //Find and delete
+    Article.findOneAndDelete({_id: articleId}, (err, articleRemoved) =>{
+      if(err){
+        return res.status(500).send({
+          status: "error",
+          message: "Error al borrar !!!"
+        });
+
+      }
+
+      if (!articleRemoved) {
+        return res.status(404).send({
+          status: "error",
+          message: "No se ha borrado el articulo. Probablemente no existe !!!"
+        });
+      }
+
+      return res.status(200).send({
+        status: "sucess",
+        article: articleRemoved
+      });
+
+    });
+  } //end delete
 
 }; //end controller
 
--- backend/routes/article.js
+++ backend/routes/article.js
@@ -12,6 +12,9 @@ router.get ("/test-de-controlador", ArticleController.test);
 //rutas utiles
 
 router.post("/save", ArticleController.save);
-
+router.get("/articles/:last?", ArticleController.getArticles);
+router.get("/article/:id", ArticleController.getArticle);
+router.put("/article/:id", ArticleController.update);
+router.delete("/article/:id", ArticleController.delete);
 
 module.exports = router;
